bf097f20c3cf3f4e996bf298fd99b02d6b626781
hang <hang22c@gmail.com>
2021-06-09 14:46:43 +0800

    ADD time delta to limiter

--- limiter/limiter_config.go
+++ limiter/limiter_config.go
@@ -19,23 +19,25 @@ const (
 	hoursPerDay = 24
 )
 
-func getTimeKeyAndDuration(timeType string, limitTimeLength int) (timeKey string, duration time.Duration) {
+func getTimeKeyAndDuration(timeType string, limitTimeLength int, hourDelta int64) (timeKey string, duration time.Duration) {
+	now := time.Now().Add(time.Hour * time.Duration(hourDelta))
+
 	switch timeType {
 	case LimitTimeTypeHour:
 		return timeType, time.Hour * time.Duration(limitTimeLength)
 	case LimitTimeTypeMinute:
 		return timeType, time.Minute * time.Duration(limitTimeLength)
 	case LimitTimeTypeHourToday:
-		return timeType + time.Now().Format("2006-01-02"), time.Hour * time.Duration(limitTimeLength)
+		return timeType + now.Format("2006-01-02"), time.Hour * time.Duration(limitTimeLength)
 	case LimitTimeTypeForever:
 		return timeType, time.Hour * 366 * hoursPerDay
 	default:
-		return time.Now().Format("2006-01-02"), time.Hour * hoursPerDay
+		return now.Format("2006-01-02"), time.Hour * hoursPerDay
 	}
 }
 
-func Available(ctx context.Context, storage LimitStorage, key, limitType, limitTimeType string, limitTimeLength int, limitAmount int64, needIncr bool) bool {
-	limiter := GetLimiter(ctx, storage, key, limitType, limitTimeType, limitTimeLength, limitAmount)
+func Available(ctx context.Context, storage LimitStorage, key, limitType, limitTimeType string, limitTimeLength int, limitAmount, hourDelta int64, needIncr bool) bool {
+	limiter := GetLimiter(ctx, storage, key, limitType, limitTimeType, limitTimeLength, limitAmount, hourDelta)
 
 	if needIncr {
 		_, _ = limiter.Incr()
@@ -46,8 +48,8 @@ func Available(ctx context.Context, storage LimitStorage, key, limitType, limitT
 	return limiter.Available()
 }
 
-func GetLimiter(ctx context.Context, storage LimitStorage, key, limitType, limitTimeType string, limitTimeLength int, limitAmount int64) Limiter {
-	timeKey, duration := getTimeKeyAndDuration(limitTimeType, limitTimeLength)
+func GetLimiter(ctx context.Context, storage LimitStorage, key, limitType, limitTimeType string, limitTimeLength int, limitAmount int64, hourDelta int64) Limiter {
+	timeKey, duration := getTimeKeyAndDuration(limitTimeType, limitTimeLength, hourDelta)
 	key = fmt.Sprintf("%s_%s", key, timeKey)
 
 	switch limitType {
